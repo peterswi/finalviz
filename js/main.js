b03e497ff42fce22db2b48129cd0b5b1fa700905
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-11-17 15:52:55 -0500

    importing correctly now

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-import Introduction from './Introduction'
-import StateStats from './StatesStats'
-import PovertyInsecurity from './PovertyInsecurity'
-import InsecurityUnemployment from './InsecurityUnemployment'
+import Introduction from './Introduction.js'
+import StateStats from './StatesStats.js'
+import PovertyInsecurity from './PovertyInsecurity.js'
+import InsecurityUnemployment from './InsecurityUnemployment.js'
 
 d3.csv('data/MMG_Master.csv',d3.autoType).then(data=>{
     console.log(data)
 
-    const intro=Introduction('.intro-Chart')
+    const intro=Introduction('.intro-map')
     const map=PovertyInsecurity('.poverty-insecurity')
     const stats=StateStats('.state-stat')
     const unemp=InsecurityUnemployment('.unemployment')
