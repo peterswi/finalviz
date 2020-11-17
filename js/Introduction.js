0837519e77c6947d6dd74f294f5776e9ff6fe6d6
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-11-17 18:26:24 -0500

    trying to get this map to show

--- js/Introduction.js
+++ js/Introduction.js
@@ -17,34 +17,49 @@ function Introduction(container){
         map.draw(d3.select('#map').datum(data));
 
 });
+
 */
-    d3.csv('data/MMG_Master.csv',d3.autoType).then(fooData=>{
-        console.log('food data',fooData)
-    
-        const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, 975, 610]);
+    const width=800
+    const height=800
+    const half=width/2
+
+    Promise.all([ // load multiple files
+        d3.json('data/states-10m.json'),
+        d3.csv('data/MMG_Master.csv',d3.autoType)
+    ]).then(data=>{ 
+        
+        const us=data[0]
+        const fooData=data[1]
+        
 
         const color = d3.scaleQuantize([1, 10], d3.schemeBlues[9])
+        const path = d3.geoPath()
 
-        svg.append("g")
-            .attr("transform", "translate(610,20)")
+        const svg = d3.select(container)
+            .append('svg')
+            .attr('width', width)  
+            .attr('height',height)
+            .attr('viewBox', [0,0,width, height])
+            .append('g')
+            .attr('transform', `translate(${width/16}, ${height/16})`)
         //    .append(() => legend({color, title: '', width: 260}));
 
         svg.append("g")
             .selectAll("path")
             .data(topojson.feature(us, us.objects.states).features)
             .join("path")
-            .attr("fill", d => color(data.get(d.id)))
+            .attr("fill", d => color(fooData.id))
             .attr("d", path)
             .append("title")
             //.text(d => `${d.properties.name}, ${states.get(d.id.slice(0, 2)).name} ${format(data.get(d.id))}`)
 
-        svg.append("path")
-        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-        .attr("fill", "none")
-        .attr("stroke", "white")
-        .attr("stroke-linejoin", "round")
-        .attr("d", path);
+
+         svg.append("path")
+            .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+            .attr("fill", "none")
+            .attr("stroke", "white")
+            .attr("stroke-linejoin", "round")
+            .attr("d", path);
 
     })
 }
