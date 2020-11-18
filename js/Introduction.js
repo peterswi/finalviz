863d75c853125a32a3f78f7818176e387625fba4
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-11-18 10:54:11 -0500

    starting on force

--- js/Introduction.js
+++ js/Introduction.js
@@ -1,24 +1,8 @@
 //WILL
 
-//import {legend} from "@d3/color-legend"
+
 
 function Introduction(container){
-
-/*
-    var map = d3.choropleth()
-        .geofile('/d3-geomap/topojson/countries/USA.json')
-        .projection(d3.geoAlbersUsa)
-        .column('Food Insecurity Rate')
-        .unitId('State Name')
-        .scale(1000)
-        .legend(true);
-
-    d3.csv('/data/MMG_Master.csv').then(data => {
-        map.draw(d3.select('#map').datum(data));
-
-});
-
-*/
 
     Promise.all([ // load multiple files
         
@@ -37,7 +21,7 @@ function Introduction(container){
             const height=600
             const half=width/2
         
-        const color = d3.scaleQuantize([1, 8], d3.schemeBlues[7])
+        const color = d3.scaleQuantize([1, 9], d3.schemeBlues[8])
             .domain(d3.extent(fiAvg, d=>d.avgFIrate))
         const projection = d3.geoAlbersUsa().fitSize([width, height], states);
         const path = d3.geoPath().projection(projection);
@@ -51,10 +35,10 @@ function Introduction(container){
             .append('svg')
             .attr('width', width)  
             .attr('height',height)
-            .attr('viewBox', [0,0,width+50, height+50])
+            .attr('viewBox', [0,0,width+150, height+150])
             .append('g')
             .attr('transform', `translate(${width/16}, ${height/16})`)
-
+             
         svg.selectAll(container)
             .data(states.features)
             .join("path")
@@ -74,10 +58,47 @@ function Introduction(container){
             .attr("stroke", "black");
         
         svg.append("g")
-            .attr("transform", "translate(460,-10)")
+            .attr("transform", "translate(560,350)")
             .call(legend)
+            
+        svg.append('text')
+            .attr('class','graphTitle')
+            .attr('x',half)
+            .attr('y',100)
+            .text("US Food Insecurity")
+            .style('text-anchor','middle')
+            .style('font-style','italic')
+            .attr('font-size',40)
 
-       
+            console.log(fiAvg)
+        const svg2 = d3.select('.chart')
+            .append('svg')
+            .attr('width', width)  
+            .attr('height',height)
+            .attr('viewBox', [0,0,width+150, height+150])
+            .append('g')
+            .attr('transform', `translate(${width/16}, ${height/16})`)
+
+        const circleScale=d3.scaleLinear()
+            .domain(d3.extent(fiAvg,d=>d.avgFInum))
+            .range([8,30])
+        
+        const stateForce = d3.forceSimulation(fiAvg)
+            .force('charge', d3.forceManyBody().strength(-40))
+            .force('center', d3.forceCenter().x(half).y(half))
+        
+        const nodeElements=svg2.selectAll('circle')
+            .data(fiAvg)
+            .enter().append('circle')
+                .attr('r', d=> circleScale(d.avgFInum)
+                    )
+                .attr('fill', d=>color(d.avgFIrate))
+        
+        stateForce.on("tick", function(){
+            nodeElements
+                .attr("cx", 300)
+                .attr("cy", 100)
+        })
     })
 }
 export default Introduction
