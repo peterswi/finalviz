7049655bbc00812ff79f2490b93f5c342275f86f
Erin Corcoran <corcorec@bc.edu>
2020-11-18 19:26:23 -0500

    got axes

--- js/StatesStats.js
+++ js/StatesStats.js
@@ -2,14 +2,14 @@
 
 // Want to answer the question: where is food insecurity getitng worse? with a candlestick chart
 // HTML container = state-stat
-const margin = ({top: 15, right: 15, bottom: 25, left: 25})
-const width = 600 - margin.left - margin.right
+const margin = ({top: 15, right: 15, bottom: 50, left: 40})
+const width = 800 - margin.left - margin.right
 const height = 600 - margin.top - margin.bottom
 
 function StateStats(container){
-    d3.csv('data/MMG_Master.csv').then(data => {
+    d3.csv('data/MMG_Avg.csv').then(data => {
         console.log(data)
-        let states = data.stateName;
+        let states = data.state;
 
         let svg = d3.selectAll(container).append('svg')
             .attr("width", width + margin.left + margin.right)
@@ -18,18 +18,41 @@ function StateStats(container){
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
         let xScale = d3.scaleOrdinal()
-            .domain(d3.extent(data, d => d.stateName))
+            .domain(d3.extent(data, d => d.state))
             .range([0,width]);
         
         let yScale = d3.scaleLinear()
-            .domain(d3.extent(data, d => d.FoodInsecurityRate))
+            .domain(d3.extent(data, d => d.avgFIrate))
             .range([height, 0])
 
         let xAxis = d3.axisBottom()
             .scale(xScale)
+            .ticks(0)
 
         let yAxis = d3.axisLeft()
             .scale(yScale)
+
+        svg.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", `translate(0, ${height})`)
+
+        svg.append("g")
+            .attr("class", "y-axis")
+
+        let yAxisGroup = svg.select(".y-axis").call(yAxis)
+        let xAxisGroup = svg.select(".x-axis").call(xAxis)
+
+
+  /*      const label = svg.append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .selectAll("text")
+            .data(data)
+            .join("g")
+           // .attr("transform", d => `translate(${xScale(d.miles)},${yScale(d.gas)})`)
+            .append("text")
+            .text(data => data.stateName) */
+
     })
 
 }
