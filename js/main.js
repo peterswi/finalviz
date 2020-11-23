857afe1e52ad69fa62b784a46cf1e72277da4dd8
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-11-23 10:08:32 -0500

    adding containers where appropriate

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ import InsecurityUnemployment from './InsecurityUnemployment.js'
 d3.csv('data/MMG_Master.csv',d3.autoType).then(data=>{
     
 
-    const intro=Introduction('.intro-map')
-    const map=PovertyInsecurity('.poverty-insecurity')
+    const intro=Introduction('.intro-map','.absolute-force','.relative')
+    const map=PovertyInsecurity('.poverty-insecurity','.chart2')
     const stats=StateStats('.state-stat')
     const unemp=InsecurityUnemployment('.unemployment')
 
