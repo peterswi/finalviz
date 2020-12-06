aa3140dd74a9fcb8fd9ac831ddb77714300bcde9
Lurein Perera <pererag@bc.edu>
2020-12-06 11:24:53 -0500

    added last comment to state highlighting of PovertyInsecurity

--- js/PovertyInsecurity.js
+++ js/PovertyInsecurity.js
@@ -133,7 +133,7 @@ Promise.all([ // load multiple files
             let stateHover = svg.selectAll('path')
               .on("mouseenter", (event, nodes) => {
                   let d = data
-                  d3.select(event.currentTarget).style('fill-opacity', 0.7)
+                  d3.select(event.currentTarget).style('fill-opacity', 0.7) // highlight the state being hovered
                   svg2.selectAll("path") // here we're finding the state on the other map and highlighting it
                     .filter(function() {
                       return d3.select(this).attr("id") == nodes.properties.NAME;
